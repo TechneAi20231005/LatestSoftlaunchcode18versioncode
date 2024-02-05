82e95748087b79ab492bd28c1b9892778c1b7bda
AsmitaMargaje <asmita.margaje@techneai.com>
2024-02-05 14:40:20 +0530

    implemented redux in master and ticket component

---
+++ src/screens/BillChecking/Slices/BillCheckingTransactionAction.js
@@ -0,0 +1,28 @@
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import GeneralSettingService from "../../services/SettingService/GeneralSettingService";
+
+// export const getGeneralSettingData = createAsyncThunk("getGeneralSettingData",async(config,thunkapi)=>{
+//     try{
+//         const service = new GeneralSettingService();
+//         const response = await service.getGeneralSetting()
+//         console.log("reGeneral",response)
+//         return (response)
+//     }catch (error){
+//         throw(error)
+//     }
+// })
+
+
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// export const getGeneralSettingData = createAsyncThunk("getGeneralSettingData",async(config,thunkapi)=>{
+//     try{
+//         const service = new GeneralSettingService();
+//         const response = await service.getGeneralSetting()
+//         return (response)
+//     }catch (error){
+//         throw(error)
+//     }
+// })
+
