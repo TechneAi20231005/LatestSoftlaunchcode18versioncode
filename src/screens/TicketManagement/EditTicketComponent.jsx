6c9e022a13c9b6fe786350906efd96d9b01b698c
AsmitaMargaje <asmita.margaje@techneai.com>
2024-03-29 13:04:59 +0530

    resolved ticketing defect

--- src/screens/TicketManagement/EditTicketComponent.jsx
+++ src/screens/TicketManagement/EditTicketComponent.jsx
@@ -360,9 +360,17 @@ export default function EditTicketComponent({ match }) {
               value: d.id,
               label: d.first_name + " " + d.last_name,
             }));
+
+          const entryUserSelect = res.payload.data.data
+            .filter((d) => d.is_active == 1)
+            .map((d) => ({
+              value: d.id,
+              label: d.first_name + " " + d.last_name,
+            }));
+
           setUser(data);
           setUserDropdown(select);
-          setUserdrp(select);
+          setUserdrp(entryUserSelect);
         }
       }
     });
