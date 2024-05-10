3b9190b0b9ed2695eedb3657b15f548b50a805b9
AsmitaMargaje <asmita.margaje@techneai.com>
2024-05-10 12:47:45 +0530

    resolved blocker issue

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -330,14 +330,7 @@ export default function HrDashboard(props) {
                         lineHeight: "revert-layer",
                       }}
                     >
-                      {" "}
-                      {`Regularization`}
-                      <br />
-                      {`Request : ${
-                        approvedNotifications?.length
-                          ? approvedNotifications?.length
-                          : 0
-                      }`}
+                      Regularization
                     </button>
                     {approvedNotifications?.length > 0 && (
                       <div
@@ -479,6 +472,10 @@ export default function HrDashboard(props) {
                               const date = ele.created_at.split(" ")[0];
                               const time = ele.created_at.split(" ")[1];
 
+                              const parts1 = ele?.url?.split("/"); // Split the string by '/'
+                              const ticketID1 =
+                                parts1 && parts1[parts1?.length - 1];
+
                               return (
                                 <li
                                   className="py-2 mb-1 border-bottom"
@@ -489,7 +486,7 @@ export default function HrDashboard(props) {
                                     style={{ cursor: "pointer" }}
                                     onClick={(e) => {
                                       handleShowApproveRequestModal();
-                                      handleRegularizationRequest(ticketID);
+                                      handleRegularizationRequest(ticketID1);
                                     }}
                                   >
                                     {ele.url && (
