d74698781c0d2047b9b69f0cac081bdde2edd37b
priyankatechneai <priyanka.satpute@techneai.com>
2024-06-03 13:27:38 +0530

    add select skeleton in report

--- src/screens/Reports/TimelineReportComponent.jsx
+++ src/screens/Reports/TimelineReportComponent.jsx
@@ -1,83 +1,101 @@
-import React, { useEffect, useState }  from "react";
-import DataTable from "react-data-table-component";
-import { Table } from "react-bootstrap";
-import ErrorLogService from "../../services/ErrorLogService";
-import ReportService from "../../services/ReportService/ReportService";
-import PageHeader from "../../components/Common/PageHeader";
-import Chart from "react-apexcharts";
+import React, { useEffect, useState } from 'react';
+
+import ErrorLogService from '../../services/ErrorLogService';
+import ReportService from '../../services/ReportService/ReportService';
+import PageHeader from '../../components/Common/PageHeader';
+import Chart from 'react-apexcharts';
 
 export default function TimelineReportComponent() {
+  const [chart, setChart] = useState(null);
 
-    const [userData,setUserData] = useState(null);
-    const [chart, setChart] = useState(null);    
-    
+  const handleForm = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    await new ReportService()
+      .getTimeLineReport(formData)
+      .then((res) => {
+        if (res.status === 200) {
+          if (res.data.status === 1) {
+            const data = res.data.data;
+            setChart(data);
+          } else {
+            setChart(null);
+          }
+        } else {
+          new ErrorLogService().sendErrorLog(
+            'TimelineReport',
+            'Get_TimelineReport',
+            'INSERT',
+            res.message
+          );
+        }
+      })
+      .catch((error) => {
+        const { response } = error;
+        const { request, ...errorObject } = response;
+        new ErrorLogService().sendErrorLog(
+          'TimelineReport',
+          'Get_TimelineReport',
+          'INSERT',
+          errorObject.data.message
+        );
+      });
+  };
 
-    const handleForm = async(e) => {
-        e.preventDefault();
-        const formData=new FormData(e.target);
-        await new ReportService().getTimeLineReport(formData).then(res=>{
-            if(res.status===200){
-                if(res.data.status===1){
-                    const data=res.data.data;
-                    setChart(data);
-                }else{
-                    setChart(null);
-                }
-            }else{
-                new ErrorLogService().sendErrorLog("TimelineReport","Get_TimelineReport","INSERT",res.message);    
-            }
-        }).catch(error => {
-            const { response } = error;
-            const { request, ...errorObject } = response; 
-            new ErrorLogService().sendErrorLog("TimelineReport","Get_TimelineReport","INSERT",errorObject.data.message);
-        })
-    }
-
-   
-    useEffect(() => {
-       
-    }, [])
+  useEffect(() => {}, []);
 
   return (
     <div className="container-xxl">
-            <PageHeader headerTitle="Timeline Report" />
+      <PageHeader headerTitle="Timeline Report" />
 
-            <div className='card mt-2'>
-                <div className='card-body'>
-                    <form onSubmit={handleForm}>
-                        <div className="row">
-                            <div className="col-md-3">
-                                <label htmlFor="" className="">
-                                    <b>Ticket Id :</b>
-                                </label>
-                                <input type="text" className="form-control form-control-sm"
-                                name="ticket_id" />
-                            </div>
-                            <div className='col-md-2'>
-                                <button className='btn btn-sm btn-warning' type="submit" style={{marginTop:'20px'}}>
-                                    <i class="icofont-search-1"></i>
-                                </button>
-                                <button className='btn btn-sm btn-info' type="button" onClick={() => window.location.reload(false)}
-                                style={{marginTop:'20px'}}><i class="icofont-refresh"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+      <div className="card mt-2">
+        <div className="card-body">
+          <form onSubmit={handleForm}>
+            <div className="row">
+              <div className="col-md-3">
+                <label htmlFor="" className="">
+                  <b>Ticket Id :</b>
+                </label>
+                <input
+                  type="text"
+                  className="form-control form-control-sm"
+                  name="ticket_id"
+                />
+              </div>
+              <div className="col-md-2">
+                <button
+                  className="btn btn-sm btn-warning"
+                  type="submit"
+                  style={{ marginTop: '20px' }}
+                >
+                  <i class="icofont-search-1"></i>
+                </button>
+                <button
+                  className="btn btn-sm btn-info"
+                  type="button"
+                  onClick={() => window.location.reload(false)}
+                  style={{ marginTop: '20px' }}
+                >
+                  <i class="icofont-refresh"></i>
+                </button>
+              </div>
             </div>
+          </form>
+        </div>
+      </div>
 
-            <div className='card mt-2'>
-                <div className='card-body'>
-
-                {chart && <Chart 
-                            options={chart.options} 
-                            series={chart.series} 
-                            type="rangeBar"
-                            height={chart.options.height} />
-                        }
-
-                </div>
-            </div>
-    </div>   
-  )
+      <div className="card mt-2">
+        <div className="card-body">
+          {chart && (
+            <Chart
+              options={chart.options}
+              series={chart.series}
+              type="rangeBar"
+              height={chart.options.height}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
-
