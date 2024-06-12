2f285993251f4ba565f6f7772d9d4a8378fe208d
priyankatechneai <priyanka.satpute@techneai.com>
2024-06-12 15:05:45 +0530

    resolved entry user and entry email issue in edit ticket

--- src/screens/TicketManagement/EditTicketComponent.jsx
+++ src/screens/TicketManagement/EditTicketComponent.jsx
@@ -887,12 +887,8 @@ export default function EditTicketComponent({ match }) {
   const loadCommentsCallback = useCallback(() => {
     loadComments();
   }, [loadComments]);
-  const option = [
-    {
-      value: 'Priyanka Dupargude',
-      label: 'Priyanka Dupargude'
-    }
-  ];
+
+
 
   const [selectedValue, setSelectedValue] = useState('');
   const [selectedCheckBoxValue, setSelectedCheckBoxValue] = useState('');
@@ -929,22 +925,8 @@ export default function EditTicketComponent({ match }) {
     }
   }, [user]);
 
-  const currentDate = new Date();
-  const formattedDate = `${currentDate.getFullYear()}-${(
-    currentDate.getMonth() + 1
-  )
-    .toString()
-    .padStart(2, '0')}-${currentDate
-    .getDate()
-    .toString()
-    .padStart(2, '0')} ${currentDate
-    .getHours()
-    .toString()
-    .padStart(2, '0')}:${currentDate
-    .getMinutes()
-    .toString()
-    .padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}`;
 
+  
   useEffect(() => {
     if (checkRole && checkRole[0]?.can_update === 0) {
       // alert("Rushi")
