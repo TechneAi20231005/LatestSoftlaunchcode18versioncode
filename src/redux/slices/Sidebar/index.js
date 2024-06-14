ce36266f23e608e00d433c134246dd2e1e9575d8
priyankatechneai <priyanka.satpute@techneai.com>
2024-06-14 11:15:05 +0530

    Integrate Redux for managing sidebar state

---
+++ src/redux/slices/Sidebar/index.js
@@ -0,0 +1,72 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { getEmployeeListThunk, getMenuListThunk } from '../../services/Sidebar';
+
+const initialState = {
+  employeeList: '',
+  getMenuList: '',
+  filterMenuList: [],
+
+  isLoading: {
+    getEmployeeList: false
+  },
+  errorMsg: {
+    getEmployeeList: ''
+  },
+  successMsg: {
+    getEmployeeList: ''
+  }
+};
+const sidebarSlice = createSlice({
+  name: 'sidebarSlice',
+  initialState,
+  reducers: {},
+
+  extraReducers(builder) {
+    builder
+      .addCase(getEmployeeListThunk.pending, (state, action) => {
+        state.isLoading.getEmployeeList = true;
+      })
+      .addCase(getEmployeeListThunk.fulfilled, (state, action) => {
+        state.isLoading.getEmployeeList = false;
+        state.employeeList = action.payload.data;
+        state.successMsg.getEmployeeList = action.payload.msg;
+      })
+      .addCase(getEmployeeListThunk.rejected, (state, action) => {
+        state.isLoading.getEmployeeList = false;
+        state.employeeList = [];
+        state.errorMsg.getEmployeeList = action.error.message;
+      })
+
+      .addCase(getMenuListThunk.pending, (state, action) => {
+        state.isLoading.getMenuList = true;
+      })
+      .addCase(getMenuListThunk.fulfilled, (state, action) => {
+        state.isLoading.getMenuList = false;
+        state.menuList = action.payload.data;
+        state.filterMenuList = state?.menuList?.menu.filter((menu) => {
+          if (state.employeeList === 'SELF') {
+            return menu;
+          } else {
+            return menu?.id === 16;
+          }
+        });
+
+        state.successMsg.getMenuList = action.payload.msg;
+      })
+      .addCase(getMenuListThunk.rejected, (state, action) => {
+        state.isLoading.getMenuList = false;
+        state.menuList = [];
+        state.errorMsg.getMenuList = action.error.message;
+      });
+  }
+});
+
+// export const {
+//   addUserPendingOrderRequest,
+//   editUserPendingOrderRequest,
+//   deleteUserPendingOrderRequest,
+//   resetUserAddedOrderList,
+//   resetPendingOrderListData
+// } = generatePoSlice.actions;
+export default sidebarSlice.reducer;
