de6a6b9218f572df845de600f1b76746ccd81a75
SwapnilSurawar <swapnil.surawar@techneai.com>
2024-12-12 14:00:54 +0530

    page header added

--- src/screens/Dashboard/PowerBidashboard.jsx
+++ src/screens/Dashboard/PowerBidashboard.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import PageHeader from '../../components/Common/PageHeader';
 
 function PowerBidashboard() {
   return (
+    <>
+      <PageHeader
+
+            headerTitle="Performance View Report"
+          />
     <div style={{ height: "100vh", overflow: "hidden" }}>
       <iframe
         src="https://app.powerbi.com/view?r=eyJrIjoiNzY3NGE2Y2ItYzM4Yi00M2IxLTgzZGQtOTI3YmMxYzk3ZmVlIiwidCI6IjJhZTgyM2ViLTE3YTYtNDIyOS1iOGNiLTFiMGI0ZDJhNDM1MyJ9"
@@ -14,6 +20,7 @@ function PowerBidashboard() {
         allowFullScreen
       ></iframe>
     </div>
+    </>
   )
 }
 
