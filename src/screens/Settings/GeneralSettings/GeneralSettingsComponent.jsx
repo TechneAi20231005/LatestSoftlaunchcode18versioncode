93334f60007c21e70ac171aad457f217ee180cca
intern <intern@techneai.com>
2024-12-13 17:53:45 +0530

    validation and formik updated

--- src/screens/Settings/GeneralSettings/GeneralSettingsComponent.jsx
+++ src/screens/Settings/GeneralSettings/GeneralSettingsComponent.jsx
@@ -25,6 +25,8 @@ import { handleModalClose, handleGeneralModal } from '../SettingSlice';
 import { customSearchHandler } from '../../../utils/customFunction';
 import SearchBoxHeader from '../../../components/Common/SearchBoxHeader ';
 import TableLoadingSkelton from '../../../components/custom/loader/TableLoadingSkelton';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { CustomValidation } from '../../../components/custom/CustomValidation/CustomValidation';
 
 function GeneralSettings() {
   //initial  state
@@ -128,52 +130,52 @@ function GeneralSettings() {
     });
   }, [dispatch]);
 
-  const handleForm = (id) => async (e) => {
-    e.preventDefault();
+  // const handleForm = (id) => async (e) => {
+  // e.preventDefault();
 
-    const userDet = userDetail?.current?.props?.value;
-    const usersettingValue = userValue?.current?.value;
+  // const userDet = userDetail?.current?.props?.value;
+  // const usersettingValue = userValue?.current?.value;
 
-    const settingName = useSetting?.current?.value;
-    const remark = useRemark?.current?.value;
-    let array = [];
+  // const settingName = useSetting?.current?.value;
+  // const remark = useRemark?.current?.value;
+  // let array = [];
 
-    // Add the value 0 to the array
-    array.push(0);
+  // // Add the value 0 to the array
+  // array.push(0);
 
-    // Assign the array to form.user_id
+  // // Assign the array to form.user_id
 
-    let arrayOfId = [];
-    for (let i = 0; i < userDet?.length; i++) {
-      arrayOfId.push(userDet[i].value);
-    }
+  // let arrayOfId = [];
+  // for (let i = 0; i < userDet?.length; i++) {
+  //   arrayOfId.push(userDet[i].value);
+  // }
 
-    const form = {};
+  // const form = {};
 
-    if (
-      settingName === 'Time Regularization after task complete' &&
-      arrayOfId?.length === 0
-    ) {
-      form.user_id = array;
-    } else {
-      form.user_id = arrayOfId;
-    }
+  // if (
+  //   settingName === 'Time Regularization after task complete' &&
+  //   arrayOfId?.length === 0
+  // ) {
+  //   form.user_id = array;
+  // } else {
+  //   form.user_id = arrayOfId;
+  // }
 
-    form.setting_name = settingName;
-    form.remark = remark;
-    form.value = usersettingValue;
-    form.is_active = true;
+  // form.setting_name = settingName;
+  // form.remark = remark;
+  // form.value = usersettingValue;
+  // form.is_active = true;
 
-    if (!id) {
-      dispatch(postGeneralSettingData(form));
-    } else {
-      dispatch(updateGeneralSettingData({ id, payload: form })).then((res) => {
-        if (res?.payload?.data?.status === 1) {
-          dispatch(getGeneralSettingData());
-        }
-      });
-    }
-  };
+  //   if (!id) {
+  //     dispatch(postGeneralSettingData(form));
+  //   } else {
+  //     dispatch(updateGeneralSettingData({ id, payload: form })).then((res) => {
+  //       if (res?.payload?.data?.status === 1) {
+  //         dispatch(getGeneralSettingData());
+  //       }
+  //     });
+  //   }
+  // };
   //columns
   const columns = [
     {
@@ -309,13 +311,99 @@ function GeneralSettings() {
     }
   ];
 
-  const handleKeyPress = (event) => {
-    // Prevent typing more than one character
-    if (event.target.value.length >= 1) {
-      event.preventDefault();
+  // const handleKeyPress = (event) => {
+  //   // Prevent typing more than one character
+  //   if (event.target.value.length >= 1) {
+  //     event.preventDefault();
+  //   }
+  // };
+
+  const fields = [
+    {
+      name: 'setting_name',
+      label: 'Setting name',
+      required: true,
+      alphaNumeric: true,
+      min: 3,
+      max: 50
+    },
+    {
+      name: 'value',
+      label: 'Value',
+      required: true,
+      max: 1,
+      alphaNumeric: false
+    },
+    {
+      name: 'user_id',
+      label: 'User name',
+      isObject: true,
+      required: true
+    },
+    {
+      name: 'remark',
+      label: 'Remark',
+      max: 1000,
+      required: false,
+      alphaNumeric: true
     }
+  ];
+
+  const validationSchema = CustomValidation(fields);
+
+  // let valueof = modal.modalData
+  //   ? filteredCountryData.find((d) => modal.modalData.country_id === d.value)
+  //   : '';
+
+  // let stateValue = modal.modalData
+  //   ? filteredStateData?.find((d) => modal.modalData.state_id === d.value)
+
+  //   : '';
+  const userData =
+    modal?.modalData?.user_id &&
+    user?.filter((d) => modal?.modalData?.user_id?.includes(d.value));
+  console.log(userData, 'userdata');
+
+  const initialValues = {
+    setting_name: modal.modalData ? modal.modalData?.setting_name : '',
+    value: modal.modalData ? modal.modalData?.value : '',
+    user_id: modal.modalData ? userData?.map((item) => item) : '',
+    remark: modal.modalData?.remark || '',
+    is_active: 1
   };
 
+  const handleForm = async (values, id) => {
+    const formData = new FormData();
+    formData.append('setting_name', values.setting_name);
+    formData.append('value', values.value);
+    // formData.append('user_id', values.user_id);
+    values?.user_id.forEach((item) => {
+      formData?.append('user_id[]', item?.value);
+    });
+    formData.append('remark', values.remark);
+
+    const editformdata = new FormData();
+    editformdata.append('setting_name', values.setting_name);
+    editformdata.append('value', values.value);
+    // editformdata.append('user_id', values.user_id);
+    values?.user_id.forEach((item) => {
+      editformdata?.append('user_id[]', item?.value);
+    });
+    editformdata.append('remark', values.remark);
+    editformdata.append('is_active', values.is_active);
+
+    if (!id) {
+      dispatch(postGeneralSettingData(formData));
+      setTimeout(() => {
+        loadData();
+      }, 500);
+    } else {
+      dispatch(updateGeneralSettingData({ id: id, payload: editformdata }));
+      setTimeout(() => {
+        loadData();
+      }, 500);
+    }
+  };
   useEffect(() => {
     loadData();
   }, [loadData]);
@@ -326,7 +414,6 @@ function GeneralSettings() {
   useEffect(() => {
     handleSearch();
   }, [searchTerm, handleSearch]);
-
   return (
     <div className="container-xxl">
       {Notify && (
@@ -406,133 +493,145 @@ function GeneralSettings() {
         //   });
         // }}
       >
-        <form
-          method="post"
-          onSubmit={handleForm(modal.modalData ? modal.modalData.id : '')}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(value) =>
+            handleForm(value, modal.modalData ? modal.modalData.id : '')
+          }
         >
-          <Modal.Header
-            closeButton
-            onClick={() => {
-              dispatch(
-                handleModalClose({
-                  showModal: false,
-                  modalData: null,
-                  modalHeader: 'Add Setting'
-                })
-              );
-            }}
-          >
-            <Modal.Title className="fw-bold">{modal.modalHeader}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="deadline-form">
-              <div className="row g-3 mb-3">
-                <div className="col-sm-12">
-                  <label className="form-label font-weight-bold">
-                    Setting Name :<Astrick color="red" size="13px" />
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm"
-                    id="setting_name"
-                    name="setting_name"
-                    maxLength={50}
-                    ref={useSetting}
-                    defaultValue={
-                      modal.modalData && modal.modalData.setting_name
-                    }
-                    required
-                    readOnly={modal.modalData ? true : false}
-                  />
-                </div>
-
-                <div className="col-sm-12">
-                  <label className="form-label font-weight-bold">Value :</label>
-                  <input
-                    type="number"
-                    className="form-control form-control-sm"
-                    id="value"
-                    name="value"
-                    maxLength={1}
-                    onKeyPress={handleKeyPress}
-                    ref={userValue}
-                    defaultValue={modal.modalData && modal.modalData.value}
-                    // readOnly={modal.modalData ? true : false}
-                  />
-                </div>
-
-                <div className="col-sm-12">
-                  <label className="form-label font-weight-bold">
-                    Select User :<Astrick color="red" size="13px" />
-                  </label>
-                  {user && (
-                    <Select
-                      id="user_id"
-                      name="user_id[]"
-                      ref={userDetail}
-                      options={user}
-                      // defaultValue={}
-                      isMulti
-                      defaultValue={
-                        modal.modalData &&
-                        user?.filter((d) =>
-                          modal.modalData.user_id.includes(d.value)
-                        )
-                      }
-                    />
-                  )}
-                </div>
-                <div className="col-sm-12">
-                  <label className="form-label font-weight-bold">
-                    Remark :
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm"
-                    id="remark"
-                    name="remark"
-                    maxLength={1000}
-                    ref={useRemark}
-                    defaultValue={modal.modalData ? modal.modalData.remark : ''}
-                  />
-                </div>
-              </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            {!modal.modalData && (
-              <button
-                type="submit"
-                className="btn btn-primary text-white"
+          {({ values, setFieldValue }) => (
+            <Form>
+              <Modal.Header
+                closeButton
+                onClick={() =>
+                  dispatch(
+                    handleModalClose({
+                      showModal: false,
+                      modalData: null,
+                      modalHeader: 'Add Setting'
+                    })
+                  )
+                }
               >
-               Submit
-              </button>
-            )}
-            {modal.modalData && (
-              <button
-                type="submit"
-                className="btn btn-primary text-white"
-              >
-                Update
-              </button>
-            )}
-            <button
-              type="button"
-              className="btn btn-danger text-white"
-              onClick={() => {
-                dispatch(
-                  handleModalClose({
-                    showModal: false,
-                    modalData: '',
-                    modalHeader: ''
-                  })
-                );
-              }}
-            >
-              Cancel
-            </button>
-          </Modal.Footer>
-        </form>
+                <Modal.Title className="fw-bold">
+                  {modal.modalHeader}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="deadline-form">
+                  <div className="row g-3 mb-3">
+                    {/* Setting Name */}
+                    <div className="col-sm-12">
+                      <label className="form-label font-weight-bold">
+                        Setting Name: <span style={{ color: 'red' }}>*</span>
+                      </label>
+                      <Field
+                        type="text"
+                        name="setting_name"
+                        className="form-control form-control-sm"
+                        readOnly={!!modal.modalData}
+                      />
+                      <ErrorMessage
+                        name="setting_name"
+                        component="div"
+                        className="text-danger small"
+                      />
+                    </div>
+
+                    {/* Value */}
+                    <div className="col-sm-12">
+                      <label className="form-label font-weight-bold">
+                        Value:
+                      </label>
+                      <Field
+                        type="number"
+                        name="value"
+                        className="form-control form-control-sm"
+                      />
+                      <ErrorMessage
+                        name="value"
+                        component="div"
+                        className="text-danger small"
+                      />
+                    </div>
+
+                    {/* Select User */}
+                    {user && (
+                      <div className="col-sm-12">
+                        <label className="form-label font-weight-bold">
+                          Select User: <span className="text-danger">*</span>
+                        </label>
+
+                        <Field
+                          component={Select}
+                          id="user_id"
+                          name="user_id"
+                          options={user}
+                          isMulti
+                          value={values.user_id}
+                          // isArray="true"
+                          onChange={(selectedOptions) =>
+                            setFieldValue('user_id', selectedOptions)
+                          }
+                        />
+
+                        <ErrorMessage
+                          name="user_id"
+                          component="div"
+                          className="text-danger small"
+                        />
+                      </div>
+                    )}
+
+                    {/* Remark */}
+                    <div className="col-sm-12">
+                      <label className="form-label font-weight-bold">
+                        Remark:
+                      </label>
+                      <Field
+                        type="text"
+                        name="remark"
+                        className="form-control form-control-sm"
+                      />
+                      <ErrorMessage
+                        name="remark"
+                        component="div"
+                        className="text-danger small"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                {!modal.modalData ? (
+                  <button type="submit" className="btn btn-primary text-white">
+                    Submit
+                  </button>
+                ) : (
+                  <button type="submit" className="btn btn-primary text-white">
+                    Update
+                  </button>
+                )}
+                <button
+                  type="button"
+                  className="btn btn-danger text-white"
+                  onClick={() =>
+                    dispatch(
+                      handleModalClose({
+                        showModal: false,
+                        modalData: '',
+                        modalHeader: ''
+                      })
+                    )
+                  }
+                >
+                  Cancel
+                </button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </div>
   );
