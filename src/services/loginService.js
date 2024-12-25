a51365c0b1fb9c82b2e6124b725e4ffadd371fa7
AsmitaMargaje <asmita.margaje@techneai.com>
2024-12-25 15:52:22 +0530

    login changes

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
-import { loginURL, _apiUrl } from '../settings/constants';
+import { loginURL } from '../settings/constants';
 console.log('loginURL', loginURL);
-const login = _apiUrl + 'login';
+
 export function getData(config, callback, errorcallback) {}
 
 export function postData(config) {
   return axios.post(
-    // loginURL,
-    login,
+    loginURL,
 
     config
   );
