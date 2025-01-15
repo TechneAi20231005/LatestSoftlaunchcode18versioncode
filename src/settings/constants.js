7fcd91eb4fc6b2b39f4b839980da39ac9d54c1f0
SwapnilSurawar <swapnil.surawar@techneai.com>
2025-01-15 17:11:09 +0530

    report API Integration and error handleer

--- src/settings/constants.js
+++ src/settings/constants.js
@@ -134,10 +134,10 @@ export const ticketUrl = {
 
 export const reportUrl = {
   ticketReport: _apiUrl + 'report/ticketReport',
-  userTaskReport: _apiUrl + 'report/userTaskReport',
+  userTaskReport: _rewampApiUrl + 'report/userTaskReport',
   ticketTimelineReport: _apiUrl + 'report/ticketTimelineReport',
-  resourcePlanningReport: _apiUrl + 'report/resourcePlanning',
-  variantsReport: _apiUrl + 'report/variantsReport'
+  resourcePlanningReport: _rewampApiUrl + 'report/resourcePlanning',
+  variantsReport: _rewampApiUrl + 'report/variantsReport'
   // hoursWiseTaskRecord:_apiUrl + "hoursWiseTaskRecord",  //Asmita - New api for timeline report chart
 };
 
