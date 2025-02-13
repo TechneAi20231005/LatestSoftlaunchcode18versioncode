d7bf75b883754ef820e1c9d815a681ab2e2b5f21
SwapnilSurawar <swapnil.surawar@techneai.com>
2025-02-13 10:12:37 +0530

    sprint history issue fixed

--- src/screens/TicketManagement/TaskManagement/TaskComponent.jsx
+++ src/screens/TicketManagement/TaskManagement/TaskComponent.jsx
@@ -599,7 +599,7 @@ export default function TaskComponent() {
       .getSprintReportById(ticketId, sprintId)
       .then((res) => {
         if (res?.data?.status) {
-          let temp = res?.data?.data;
+          let temp = res?.data?.data?.data;
           setSprintReport(temp);
           setShowSprintReport(true);
           let exportSprintReport = [];
@@ -749,7 +749,7 @@ export default function TaskComponent() {
     },
     {
       name: 'Task Start Date',
-      selector: (row) => row?.task_start_Date,
+      selector: (row) => row?.task_start_date,
       sortable: true,
       width: '150px'
     },
@@ -768,7 +768,7 @@ export default function TaskComponent() {
     },
     {
       name: 'Task schedule hours',
-      selector: (row) => row?.task_scheduled_Hours,
+      selector: (row) => row?.task_scheduled_hours,
       sortable: true,
       width: '150px'
     },
@@ -786,7 +786,7 @@ export default function TaskComponent() {
     },
     {
       name: 'Task actual status',
-      selector: (row) => row?.task_actual_status,
+      selector: (row) => row?.actual_status,
       sortable: true,
       width: '150px'
     }
