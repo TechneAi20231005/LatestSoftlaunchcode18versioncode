6db62a56b49f37c24f59daa3a64c08505cf011b0
SwapnilSurawar <swapnil.surawar@techneai.com>
2025-02-24 13:04:27 +0530

    updated changes

--- src/services/MastersService/StatusService.js
+++ src/services/MastersService/StatusService.js
@@ -5,6 +5,7 @@ import { masterURL } from '../../settings/constants';
 const _URL = masterURL.status;
 
 const _getAllStatus = `${_URL}/getData?type=1?export=1`;
+const _getAllGridStatus = `${_URL}/getData?export=1`;
 //  _URL + '/getData';
 const _postStatus = _URL + '/postData';
 const _getStatusById = _URL + '/getStatusById/';
@@ -41,6 +42,20 @@ export default class StatusService {
     };
 
     return axios.get(_getAllStatus, config);
+  }
+
+  getGridStatus(id) {
+    const token = localStorage.getItem('jwt_token');
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    };
+
+    return axios.get(_getAllGridStatus, config);
   }
 
   postStatus(payload) {
