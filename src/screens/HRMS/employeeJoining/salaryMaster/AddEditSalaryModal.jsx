3fd38df23b139d629459662a337cf05850d84390
Amit Solanki <amit.solanki@techneai.com>
2025-02-24 12:04:40 +0530

    updated designation and department key in salary master:HRMS

--- src/screens/HRMS/employeeJoining/salaryMaster/AddEditSalaryModal.jsx
+++ src/screens/HRMS/employeeJoining/salaryMaster/AddEditSalaryModal.jsx
@@ -27,8 +27,6 @@ function AddEditSalaryModal({ show, close, type, currentSalaryData }) {
   // // initial state
   const dispatch = useDispatch();
 
-  console.log('edit data', currentSalaryData);
-
   const addEditSalaryInitialValue = {
     department_id: type === 'EDIT' ? currentSalaryData?.department_id : '',
     designation_id: type === 'EDIT' ? currentSalaryData?.designation_id : '',
