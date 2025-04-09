42d8ffd8b13d6ba94bf689169ac3952c69c12e7e
Rohit Ghadage <rohit.ghadage@techneai.com>
2025-04-09 13:31:04 +0530

    added-to-from-masters

--- src/screens/Masters/JobRoleMaster/JobRoleComponent.jsx
+++ src/screens/Masters/JobRoleMaster/JobRoleComponent.jsx
@@ -189,21 +189,29 @@ function JobRoleMasterComponent() {
       }
     },
     {
-      accessorFn: (originalRow) => {
-        return moment(originalRow.created_at).startOf('day').toDate();
-      },
       header: 'Created At',
-      filterVariant: 'date',
-      Cell: ({ cell }) =>
-        moment(cell.row.original.created_at).format('MM/DD/YYYY HH:mm:ss')
+      accessorKey: 'created_at',
+      filterVariant: 'date-range',
+      accessorFn: (row) => new Date(row.created_at),
+      Cell: ({ row }) =>
+        row.original.created_at
+          ? moment(row.original.created_at).format('MM/DD/YYYY HH:mm:ss')
+          : '--',
+      size: 350
     },
     {
       accessorKey: 'created_by',
       header: 'Created By'
     },
     {
-      accessorFn: (originalRow) => originalRow?.updated_at || '--',
-      header: 'Updated At'
+      accessorKey: 'updated_at',
+      header: 'Updated At',
+      filterVariant: 'date-range',
+      accessorFn: (row) => new Date(row.updated_at),
+      Cell: ({ row }) =>
+        row?.original?.updated_at?.trim()
+          ? moment(row.original.updated_at).format('MM/DD/YYYY HH:mm:ss')
+          : '--'
     },
     {
       accessorFn: (originalRow) => originalRow?.updated_by || '--',
