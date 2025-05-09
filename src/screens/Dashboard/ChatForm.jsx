5ff695711d88ca33d834e543a31ebc40d5f90d41
SwapnilSurawar <swapnil.surawar@techneai.com>
2025-05-09 18:43:18 +0530

    new changes

--- src/screens/Dashboard/ChatForm.jsx
+++ src/screens/Dashboard/ChatForm.jsx
@@ -1,7 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import SendIcon from '@mui/icons-material/Send';
+import IconButton from '@mui/material/IconButton';
+import MicIcon from '@mui/icons-material/Mic';
+import { Tooltip } from '@mui/material';
 
 function ChatForm({ setChatHistory }) {
   const inputRef = useRef();
+  const [inputValue, setInputValue] = useState('');
+  const [showSend, setShowSend] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    setShowSend(e.target.value.trim() !== '');
+  };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const userMessage = inputRef.current.value.trim();
@@ -11,6 +23,8 @@ function ChatForm({ setChatHistory }) {
       ...history,
       { role: 'user', text: userMessage }
     ]);
+    setInputValue('');
+    setShowSend(false);
     setTimeout(() => {
       setChatHistory((history) => [
         ...history,
@@ -18,6 +32,7 @@ function ChatForm({ setChatHistory }) {
       ]);
     }, 600);
   };
+
   return (
     <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
       <input
@@ -25,9 +40,15 @@ function ChatForm({ setChatHistory }) {
         type="text"
         placeholder="Message..."
         className="message-input"
+        value={inputValue}
+        onChange={handleInputChange}
         required
       />
-      <button className="material-symbols-rounded">arrow_upward</button>
+       <Tooltip placement='top' title={showSend ? "Send" : "Voice Input"} arrow>
+      <IconButton>
+        {showSend ? <SendIcon fontSize='small'/> : <MicIcon  fontSize='medium'/>}
+      </IconButton>
+      </Tooltip>
     </form>
   );
 }
