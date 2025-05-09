510c75a7e3fe9290d68b68b910a09e19e20c9b08
SwapnilSurawar <swapnil.surawar@techneai.com>
2025-05-09 19:26:01 +0530

    updated

--- src/screens/TestCases/TestDraft/TestDraftDetails.jsx
+++ src/screens/TestCases/TestDraft/TestDraftDetails.jsx
@@ -1653,7 +1653,7 @@ function TestDraftDetails(props) {
       enableSorting: false
     },
     {
-      accessorFn: (originalRows) => `${originalRows?.group_name || '--'} `,
+      accessorFn: (originalRows) => `${originalRows?.testing_group || '--'} `,
       header: 'Testing Group',
       Header: (
         <span>
