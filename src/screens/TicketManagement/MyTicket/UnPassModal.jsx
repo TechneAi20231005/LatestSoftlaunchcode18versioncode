f2be3700d7db362ae3593f07687834e15f498e09
SwapnilSurawar <swapnil.surawar@techneai.com>
2025-06-16 13:08:14 +0530

    Refactor ticket management components to include pagination prop and improve code consistency

--- src/screens/TicketManagement/MyTicket/UnPassModal.jsx
+++ src/screens/TicketManagement/MyTicket/UnPassModal.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Modal } from 'react-bootstrap'
-import { errorHandler } from '../../../utils'
-import MyTicketService from '../../../services/TicketService/MyTicketService'
-import { toast } from 'react-toastify'
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import { errorHandler } from '../../../utils';
+import MyTicketService from '../../../services/TicketService/MyTicketService';
+import { toast } from 'react-toastify';
 
 const UnPassModal = ({
   remarkModal,
   handleRemarkModal,
-  setPagination=() => {},
+  setPagination = () => {},
   setColumnFilters = () => {},
   setRowSelection = () => {},
+  pagination
 }) => {
-
-  console.log("hello from unpass modal", remarkModal)
+  console.log('hello from unpass modal', pagination);
 
   const handlePassTicketForm = async (e) => {
-    try{
-      e.preventDefault()
-      const formData = new FormData(e.target)
+    try {
+      e.preventDefault();
+      const formData = new FormData(e.target);
       if (remarkModal && Array.isArray(remarkModal.modalData)) {
         remarkModal.modalData.forEach((row, index) => {
           formData.append(`id[${index}]`, row?.id);
@@ -26,109 +26,105 @@ const UnPassModal = ({
         formData.append('id[]', remarkModal.modalData.id);
       }
       formData.append('pass_status', remarkModal.status);
-     const response = await new MyTicketService().passTicket(formData);
-     if(response?.status === 200){
-      const { status, message } = response.data;
-      if(status === 1){
+      const response = await new MyTicketService().passTicket(formData);
+      if (response?.status === 200) {
+        const { status, message } = response.data;
+        if (status === 1) {
+          handleRemarkModal({
+            showModal: false,
+            modalData: '',
+            modalHeader: ''
+          });
+          toast.success(message);
+          setColumnFilters([]);
+          setRowSelection({});
+          setPagination({
+            pageIndex: pagination?.pageIndex,
+            pageSize: pagination?.pageSize
+          });
+        } else {
+          toast.error(message);
+        }
+      }
+    } catch (error) {
+      errorHandler(error);
+    }
+  };
+  return (
+    <Modal
+      centered
+      show={remarkModal.showModal}
+      onHide={(e) => {
         handleRemarkModal({
           showModal: false,
           modalData: '',
           modalHeader: '',
-        })
-         toast.success(message);
-         setColumnFilters([])
-         setRowSelection({})
-         setPagination({
-          pageIndex: 0,
-          pageSize: 10
-         })
-
-      }else{
-        toast.error(message);
-      }
-     }
-
-    }catch(error){
-      errorHandler(error)
-    }
-  }
-  return (
-    <Modal
-        centered
-        show={remarkModal.showModal}
-        onHide={(e) => {
-          handleRemarkModal({
-            showModal: false,
-            modalData: '',
-            modalHeader: '',
-            status: remarkModal.status
-          });
-        }}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title className="fw-bold">
-            {remarkModal.status === 'PASS' ? 'PASS TICKET ' : 'REJECT TICKET'}
-          </Modal.Title>
-        </Modal.Header>
-        <form onSubmit={handlePassTicketForm}>
-          <Modal.Body>
-            <div className="deadline-form">
-              <div className="row g-3 mb-3">
-                <div className="col-sm-12">
-                  <label className="form-label font-weight-bold">
-                    Ticket Id :
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm"
-                    value={
-                      Array.isArray(remarkModal.modalData)
-                        ? remarkModal.modalData?.map((row) => row.ticket_id).join(', ')
-                        : remarkModal.modalData?.ticket_id
-                    }
-                      readOnly={
-                      remarkModal?.modalData?.length <= 0 ? false : true
-                    }
-                    required
-                  />
-                </div>
-                <div className="col-sm-12">
-                  <label className="form-label font-weight-bold">
-                    Remark :*
-                  </label>
-                  <input
-                    type="text"
-                    name="remark"
-                    id="remark"
-                    className="form-control form-control-sm"
-                    required
-                    maxLength={1000}
-                  />
-                </div>
+          status: remarkModal.status
+        });
+      }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="fw-bold">
+          {remarkModal.status === 'PASS' ? 'PASS TICKET ' : 'REJECT TICKET'}
+        </Modal.Title>
+      </Modal.Header>
+      <form onSubmit={handlePassTicketForm}>
+        <Modal.Body>
+          <div className="deadline-form">
+            <div className="row g-3 mb-3">
+              <div className="col-sm-12">
+                <label className="form-label font-weight-bold">
+                  Ticket Id :
+                </label>
+                <input
+                  type="text"
+                  className="form-control form-control-sm"
+                  value={
+                    Array.isArray(remarkModal.modalData)
+                      ? remarkModal.modalData
+                          ?.map((row) => row.ticket_id)
+                          .join(', ')
+                      : remarkModal.modalData?.ticket_id
+                  }
+                  readOnly={remarkModal?.modalData?.length <= 0 ? false : true}
+                  required
+                />
+              </div>
+              <div className="col-sm-12">
+                <label className="form-label font-weight-bold">Remark :*</label>
+                <input
+                  type="text"
+                  name="remark"
+                  id="remark"
+                  className="form-control form-control-sm"
+                  required
+                  maxLength={1000}
+                />
               </div>
             </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-            <button
-              type="button"
-              className="btn btn-danger text-white"
-              onClick={() => {
-                handleRemarkModal({
-                  showModal: false,
-                  modalData: '',
-                  modalHeader: ''
-                });
-              }}
-            >
-              Cancel
-            </button>
-          </Modal.Footer>
-        </form>
-      </Modal>
-  )
-}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger text-white"
+            onClick={() => {
+              handleRemarkModal({
+                showModal: false,
+                modalData: '',
+                modalHeader: ''
+              });
+            }}
+          >
+            Cancel
+          </button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  );
+};
 
-export default UnPassModal
+export default UnPassModal;
